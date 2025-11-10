5a3072b07359fe27ee906caacf4559c2c905dd74
Arda Arslan <arda.arslan@boun.edu.tr>
2025-11-10 20:07:36 +0300

    New architectures introduced

--- architectures/__init__.py
+++ architectures/__init__.py
@@ -1,6 +1,6 @@
 from .abstract_architecture_builder import AbstractArchitectureBuilder
 from .architecture_builder import ArchitectureBuilder
-from .architecture import Architecture
+from .architecture import Architecture, SingleGCN, SingleMLP, GCNHeaded, GCNPreproject
 from .director import Director
 
-__all__ = ["AbstractArchitectureBuilder", "ArchitectureBuilder", "Architecture", "Director"]
+__all__ = ["AbstractArchitectureBuilder", "ArchitectureBuilder", "Director", "Architecture", "SingleGCN", "SingleMLP", "GCNHeaded", "GCNPreproject"]
